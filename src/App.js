3f776880d0f255353ab42800cb01571d50cb244c
jerhup <jeremy.huppe@extendhealth.com>
2017-12-20 14:29:55 -0700

    Completed the majority of the features. Still need to be able to add new cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,95 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
+const Card = (props) => {
+  return (
+    <div style={{margin: '1em'}}>
+      <img width="75" src={props.avatar_url} /> 
+    
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div stule={{fontSize: '1.25em', fontWeight: 'bold'}}>
+          {props.name}
+        </div>
+        <div>{props.company}</div>
+
+     </div>
+    </div>
+  );
+};
+
+let data = [
+  { name: "Paul O'Shannessy",
+    avatar_url: "https://avatars.githubusercontent.com/u/8445?v=3",
+    company: "Facebook"}, 
+  { name: "Ben Alpert",
+    avatar_url: "https://avatars.githubusercontent.com/u/6820?v=3",
+    company: "Facebook"
+  }
+]
+const CardList = (props) => {
+  return (
+    <div>
+      <Form />
+     {props.cards.map(card => <Card {...card} />)}
+    </div>
+  );
+}
+
+class Form extends React.Component {
+  state = { userName: ''}
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log('Event: Form Submit', this.event.target.value)
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        this.props.onSubmit(resp.data);
+      });
+  };
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" 
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value})}
+          placeholder="Github username" required />
+        <button type="submit">Add card</button>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    cards: [
+      {
+        name: "Paul O'Shannessy",
+        avatar_url: "https://avatars.githubusercontent.com/u/8445?v=3",
+        company: "Facebook"
+      },
+      {
+        name: "Sophie Alpert",
+        avatar_url: "https://avatars.githubusercontent.com/u/6820?v=3",
+        company: "Facebook"},
+    ]
+  };
+
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Form onSubmit={this.addNewCard} />
+        <CardList cards={this.state.cards} />
       </div>
     );
   }
 }
+
 
 export default App;
